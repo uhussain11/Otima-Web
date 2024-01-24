5a8c39038d829f20634ea429e6ae0a75175dc52f
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-01-24 17:02:54 -0500

    login page

--- client/src/Schedule Meeting/main.js
+++ client/src/Schedule Meeting/main.js
@@ -2,7 +2,7 @@ import * as jose from 'jose'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './main.css'
-
+import Login from '../components/login'
 import React, {useState, useEffect} from 'react'
 const SERVER = 'http://localhost:8080/api'
 
@@ -11,49 +11,6 @@ function Main(){
     const [date, changeDate] = useState(new Date());
     const [loggedIn, setLoggedIn] = useState(false)
     const [name, setName] = useState('tester')
-
-    useEffect(() =>{
-        if(!loggedIn){
-            window.google.accounts.id.initialize({
-                client_id: '131856816778-kgbf58dn5r2uql5fgdvmmrvcmb40ded4.apps.googleusercontent.com',
-                callback: responseGoogle
-              });
-              window.google.accounts.id.renderButton(
-                  document.getElementById('buttonDiv'),
-                  {theme: "outline", size: "large"}
-              )
-              window.google.accounts.id.prompt();
-        }
-      }, [window.performance.navigation.type]) 
-
-    const responseGoogle = response =>{
-        try{
-            setLoading(true);
-            const code = response;
-
-            const data = jose.decodeJwt(code.credential)
-            console.log(data)
-            setName(data.name);
-
-            fetch(`${SERVER}/login/`, {
-                method: 'POST',
-                mode: 'cors',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({data})
-              })
-              .then((res) => res.json())
-              .then((data)=>{
-                setLoggedIn(true);
-                console.log(data)
-              });
-        }
-        catch{
-            alert("Something went wrong on our end. Please try again later")
-            return;
-        }
-    }
 
     function changeValue(val) {
         changeDate(val);
@@ -85,21 +42,13 @@ function Main(){
                     <p>This Meeting will be brief (15-30 minutes)</p>
                     <p>Availability is Tues, Thur, Sat, Sun; <strong>9am-8pm</strong> EST</p>
                     <input className='submit-btn' type="submit" value={"Schedule"} />
-                </form>:  
-                <section action="" id='login'>
-                    <form className='manual' action="">
-                        <div className='name'>
-                            <input type="text" placeholder='First Name' required/>
-                            <input type="text" placeholder='Last Name' required/>
-                        </div>
-                        <input type="email" placeholder='Email' required/>
-                        <input type="password" placeholder='Password' required/>
-                        <input type="password" placeholder='Confrim Password' required/>
-                        <input type="submit" value='Continue' />
-                    </form>
-                    <div>or</div>
-                    <div id='buttonDiv'></div>
-                </section>
+                </form>:
+                <Login
+                loggedIn = {loggedIn}
+                setLoading = {setLoading}
+                setName = {setName}
+                setLoggedIn = {setLoggedIn}
+                />
             }
         </section>
     )
