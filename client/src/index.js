d3831ab50a69a9c88d2038f356cd96783b2c3a2a
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-01-25 09:27:18 -0500

    trying to setup cookies

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <CookiesProvider defaultSetOptions={{ path: '/' }}>
+        <App />
+    </CookiesProvider>
 );
 
 reportWebVitals();
