f4151f7a3eacdec614eabfb7d7537d2c7a085beb
uhussain11 <uhussain@ufl.edu>
2024-01-28 12:31:19 -0500

    about page

--- client/src/About/main.jsx
+++ client/src/About/main.jsx
@@ -1,15 +1,25 @@
 import './main.css'
 import React from 'react';
 
-function Test(){
-    // Refer to other pages if you get stuck
-
-    return(
-        <section>
-            <h1>ABOUT US</h1>
-        </section>
-    )
+const AboutUs = () => {
+    return (
+        <div>
+            <div className='contact-button'>Contact Us</div>
+            <div className='purple-header'></div>
+            <div className="heading"><h1>About Otima</h1></div>
+            <div className='text-box'>
+                <div className='h2'><h2>Our Story</h2></div>
+                <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras in placerat libero. 
+                Aenean imperdiet et magna vitae elementum. Curabitur sed finibus ante, nec fermentum libero. Maecenas efficitur, arcu ut hendrerit pharetra, 
+                elit augue pulvinar magna, a consectetur eros quam at risus. Nunc id dui vitae orci porta dapibus in eget dui. Etiam egestas, nulla at varius elementum, 
+                nibh mauris cursus nibh, ut fermentum massa justo sit amet risus.</p>
+                <div className='h2'><h2>Our Goal</h2></div>
+                <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras in placerat libero. 
+                Aenean imperdiet et magna vitae elementum. Curabitur sed finibus ante, nec fermentum libero. Maecenas efficitur, arcu ut hendrerit pharetra, 
+                elit augue pulvinar magna, a consectetur eros quam at risus. Nunc id dui vitae orci porta dapibus in eget dui. Etiam egestas, nulla at varius elementum, 
+                nibh mauris cursus nibh, ut fermentum massa justo sit amet risus.</p>
+            </div>
+        </div>
+    );
 }
-
-export default Test;
-
+export default AboutUs
