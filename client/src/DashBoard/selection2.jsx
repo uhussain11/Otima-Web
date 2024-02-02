daede7f5ec3fed2a4aff3c0bb372cceedf146895
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2024-02-01 22:08:41 -0500

    updated tickets

--- client/src/DashBoard/selection2.jsx
+++ client/src/DashBoard/selection2.jsx
@@ -3,17 +3,26 @@ import Ticket from "../components/ticket";
 import './section2.css'
 
 function Selection2({ data }) {
-    
+    const [dropDown, setDropDown] = useState(null)
 
+    function handleTicket(){
+        return false;
+    }
 
     return (
       <section id="tickets">
         <div className={data === null ? 'loading' : 'loaded'}>
+            <form className="new-ticket" onSubmit={handleTicket}>
+                <h2>Submit Ticket</h2>
+                <textarea  className="text" placeholder="Write Request"></textarea>
+                <input className="ticket-btn" type="submit" value={'Send'} />
+            </form>
           {data !== null && (
             <React.Fragment>
               <div className="tickets">
                 {data.map((ticket) => (
-                    <Ticket key={ticket.id} id={ticket.id} user={ticket.user} message={ticket.message} status={ticket.status} />
+                    <Ticket key={ticket.id} id={ticket.id} user={ticket.user} message={ticket.message} status={ticket.status}
+                    dropDown={dropDown} setDropDown={setDropDown}/>
                 ))}
               </div>
             </React.Fragment>
--- client/src/components/ticket.jsx
+++ client/src/components/ticket.jsx
@@ -1,12 +1,31 @@
 import './ticket.css'
 
-function Ticket({id, message, status}){
+function Ticket({id, message, status, setDropDown, dropDown}){
+
+    function updateStatus(){
+        if(dropDown === id){
+            setDropDown(null)
+        }else{
+            setDropDown(id)
+        }
+    }
 
     return(
         <div className="card">
             <dl className="header">
-                <h2>ID: {id}</h2>
-                <p className={status}>{status}</p>
+                <h2>Ticket ID: {id}</h2>
+                <div onClick={()=>{updateStatus()}} className={status}>
+                    <div className='toggle'>
+                        <p className='status'>{status}</p>
+                        <span id={dropDown!==id ?'toggle-symbol':'toggled-symbol'} class="material-symbols-outlined">arrow_drop_down</span>
+                    </div>
+                    <div className={dropDown === id?'dropdown':'hide-dropdown'}>
+                        <p>Resolve</p>
+                        <p>Discard</p>
+
+                    </div>
+                    
+                </div>
             </dl>
             <p className="message">{message}</p>
         </div>
